df34fd9a26dd22f6a0770e74591d21821d17883e
Nikola Rusakov <nikola.rusakov@flowup.cz>
2019-03-31 03:46:43 +0200

    feat(oauth2): redirect page WIP

--- src/app/containers/redirect/redirect.component.ts
+++ src/app/containers/redirect/redirect.component.ts
@@ -1,35 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { select, Store } from '@ngrx/store';
-import { $selectAuthToken, State } from '@app/reducers';
+import {  State } from '@app/reducers';
 import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'qv-redirect',
   templateUrl: './redirect.component.html',
 })
-export class RedirectComponent {
-  token$ = this.store.pipe(select($selectAuthToken)).pipe(
-    map(token => token)
-  );
+export class RedirectComponent implements OnInit {
+  // token$ = this.store.pipe(select($selectAuthToken)).pipe(
+  //   map(token => token)
+  // );
 
   constructor(
     private route: ActivatedRoute,
-    private store: Store<State>,
-    // private readonly init: InitService,
+    private store: Store<State>, // private readonly init: InitService,
   ) {
     console.log(this.route.snapshot.paramMap.get('token'));
     const token = this.route.snapshot.paramMap.get('token');
     // this.init.channel.port1.onmessage = onMessage;
-
-    window.postMessage({token}, location.origin/*, [this.init.channel.port2]*/);
-    window.close();
+    window.postMessage(
+      { token },
+      location.origin /*, [this.init.channel.port2]*/,
+    );
+    // window.close();
     /* function onMessage(e) {
        console.log('close redirect window');
        console.log(e);
-       return e;
-     }
+       return e;*/
+  }
+  ngOnInit(): void {
+    const token = this.route.snapshot.paramMap.get('token');
+    console.log(window);
+    console.log(parent);
+    setTimeout(() => {
+      window.postMessage({ token }, location.origin);
+    }, 2000);
 
-     console.log(parent);*/
+    // window.postMessage(
+    //   { token },
+    //   location.origin /*, [this.init.channel.port2]*/,
+    // );
+    console.log(location.origin);
+  }
+  forwardMessage(): void {
+    const token = this.route.snapshot.paramMap.get('token');
+
+    window.postMessage(
+      { token },
+      location.origin /*, [this.init.channel.port2]*/,
+    );
   }
 }
